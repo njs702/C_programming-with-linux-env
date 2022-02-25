61958acd7956ca84e4ae7795e1383fb33708801b
junseok <njs702@ajou.ac.kr>
2022-02-25 10:49:33 +0900

    cmake with window_env practice

---
+++ cmake_window/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/USER/project/cmake_window
+# Build directory: C:/Users/USER/project/cmake_window/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
